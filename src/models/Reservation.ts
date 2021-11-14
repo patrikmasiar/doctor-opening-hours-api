dad381228cbe43dd79cdcf839a8f7cd2afab9a39
patrikmasiar <masiar.patrik@gmail.com>
2021-11-14 14:46:04 +0100

    Use schema and models directly in routes file

--- src/models/Reservation.ts
+++
@@ -1,6 +0,0 @@
-import mongoose from 'mongoose';
-import reservationSchema from 'schema/Reservation';
-
-const ReservationModel = mongoose.model('Reservation', reservationSchema);
-
-export default ReservationModel;
