1090a2637222b92d08912782e262bb8e88bca294
patrikmasiar <masiar.patrik@gmail.com>
2021-11-14 14:46:04 +0100

    Use schema and models directly in routes file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,34 +2,37 @@ import express, {
   Request,
   Response,
 } from 'express';
-import ReservationModel from 'models/Reservation';
+import mongoose from 'mongoose';
 import { Reservation } from 'types/Reservation';
 
 const router = express.Router();
 
-router.get('reservation/all', async (req: Request, res: Response) => {
-  res.status(200).json({
-          error: null,
-          data: "Hello world"
-        });
-  // ReservationModel.find({}, (err, found) => {
-  //   if (!err) {
-  //     res.status(200).json({
-  //       error: null,
-  //       data: found,
-  //     });
-  //   } else {
-  //     res.status(200).json({
-  //       error: "No reservations found",
-  //       data: null,
-  //     });
-  //   }
-  // });
+const reservationSchema = new mongoose.Schema({
+  date: String,
+  start: String,
+  end: String,
+});
+
+const ReservationModel = mongoose.model('Reservation', reservationSchema);
+
+router.get('/reservation/all', async (req: Request, res: Response) => {
+  ReservationModel.find({}, (err, found) => {
+    if (!err) {
+      res.status(200).json({
+        error: null,
+        data: found,
+      });
+    } else {
+      res.status(200).json({
+        error: "No reservations found",
+        data: null,
+      });
+    }
+  });
 });
 
 router.post('/reservation', async (req: Request, res: Response) => {
   const reservationData: Reservation = req.body.reservation;
-
   const reservation = new ReservationModel(reservationData);
 
   reservation.save().then(() => {
--- src/schema/Reservation.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const reservationSchema = new mongoose.Schema({
-  date: String,
-  start: String,
-  end: String,
-});
-
-export default reservationSchema;
